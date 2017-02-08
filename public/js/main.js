f4811726ab9bee2a811cf43290f8679c0cc9bb3b
Alex Bahler <alexbahler@gmail.com>
2017-02-08 02:16:57 +0000

    Add comments

--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,31 @@ let pictionary = () => {
     let canvas, context;
     
     let draw = (position) => {
-        context.beginPath();
+        // Alert `context` to the fact that we're beginning to draw a new object
+        context.beginPath();                    
+        // Actually draw the arc
         context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
+        // Fill in the path with solid black circle
         context.fill();
     };
     
     canvas = $('canvas');
-    context = canvas[0].getContext('2d');
+    // Create drawing context for the canvas. This object allows us to draw graphics.
+    context = canvas[0].getContext('2d');       
+                                    
+    // Set width and height for correct resolutions
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
     
     // Handler callback gets an event object
     canvas.on('mousemove', (ev) => {
+        // Grab the current offset
         let offset = canvas.offset();
+        
         let newPosition = {
+            // The subtractions get us the position of the mouse relative to the top-left of the canvas
+            // Top left is {x; 0, y: 0}. 
+            // Bottom right is total width and heigh in pixels (for x and y, respectively).
             x: ev.pageX - offset.left,
             y: ev.pageY - offset.top
         };
